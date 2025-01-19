6f479e1c3165b3333012f4035fa9a815d9f69882
Gabo <slgabrielalejandro@gmail.com>
2025-01-19 17:55:57 -0400

    cree un contador como componente

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<Contador></Contador>
 </template>
 
+
+<script setup>
+import Contador from './components/Contador.vue';
+
+</script>
+
+
+
+
 <style scoped>
+
+
+
+/* 
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -32,6 +27,7 @@ header {
 }
 
 nav {
+
   width: 100%;
   font-size: 12px;
   text-align: center;
@@ -81,5 +77,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+}*/
 </style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>Bienvenido al contador:</h1>
+    <p>{{ contador }}</p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    
+</template>
+  
+  
+<script setup>
+  import { ref } from 'vue';
+  const contador = ref(0);
+  const incrementar = () => contador.value++;
+  const decrementar = () => contador.value--;
+  
+</script>
+  
+<style scoped>
+  
+  h1 {
+    color: #fff;
+  }
+  
+  p{
+    color: #fff;
+    font-size: 2rem;
+    text-align: center;
+    margin: 20px;
+  }
+  
+  button {
+    background-color: #333;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+    margin: 10px;
+  }
+</style>
+  
